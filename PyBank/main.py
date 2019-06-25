1ab39aa82201ec8df3f7ee39ea04a1672e9dd5a0
Han Yang <hanyang@Hans-MacBook-Air.local>
2019-06-24 23:07:12 -0500

    pybank final version

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,6 +2,7 @@ import os
 import csv
 csvpath=os.path.join('budget_data.csv')
 max_increase = 0
+max_decrease = 0
 with open(csvpath, newline='') as csvfile:
      csvreader=csv.reader(csvfile, delimiter=',')
      print(csvreader)
@@ -13,22 +14,21 @@ with open(csvpath, newline='') as csvfile:
         total_month+=1
         profit_loss_list.append(int(row[1]))
      #print(profit_loss_list)
-     print(total_month)
+     print('Financial Analysis')
+     print('-----------------------------')
+     print(f'Total Months: {total_month}')
      total_amount=sum(profit_loss_list)
-     print(total_amount)
-     average_change=(profit_loss_list[-1]-profit_loss_list[0])/total_month
-     print(average_change)
+     print(f'Total: ${total_amount}')
+     average_change=round(total_amount/total_month,2)
+     print(f'Average Change: ${average_change}')
      max_increase=max(profit_loss_list)
      max_decrease=min(profit_loss_list)
-     print(max_increase)
-     print(max_decrease)
-     csvreader=csv.reader(csvfile, delimiter=',')
-     for row in csvreader:
-         if row[1]==str(max_increase):
-            print(row[0])
-
+     #print(max_increase)
+     #print(max_decrease)
 with open(csvpath, newline='') as csvfile:
      csvreader=csv.reader(csvfile, delimiter=',')
      for row in csvreader:
          if row[1]==str(max_increase):
-            print(row[0])
+            print(f'Greatest Increase in Profit: {row[0]} (${max_increase})')
+         elif row[1]==str(max_decrease):
+            print(f'Greatest Decrease in Profit: {row[0]} (${max_decrease})')
